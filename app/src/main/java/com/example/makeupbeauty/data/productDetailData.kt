6977e89eab8e1d6ec76592e6356f9e0bb5ae493c
zdd0419 <19301080@bjtu.edu.cn>
2021-12-25 15:11:24 +0800

    完善data模板

---
+++ app/src/main/java/com/example/makeupbeauty/data/productDetailData.kt
@@ -0,0 +1,22 @@
+package com.example.makeupbeauty.data
+
+object productDetailData {
+ val kh= mutableListOf(
+     productDeatil(
+         1,
+         "【圣诞礼物】完美日记银翼小细跟滋润口红许愿星限定唇膏唇釉秋冬",
+         109.00,
+         1,
+         "https://s2.loli.net/2021/12/24/uFkHI9ySnd4JRsr.jpg",
+         "https://s2.loli.net/2021/12/24/KULNBy46R2ZkAhT.jpg",
+         "https://s2.loli.net/2021/12/24/HQP9tcweAzJ4k6f.jpg",
+         "https://s2.loli.net/2021/12/24/bxVlwEOTYn7A8NZ.jpg",
+         "https://s2.loli.net/2021/12/24/bjVUGavYQh3fJsB.jpg",
+         "https://s2.loli.net/2021/12/24/y6LezZT3U1khqPA.jpg",
+         "苹果红",
+         "银冀红棕",
+         "液体玫瑰",
+         "麋鹿棕"
+     )
+ )
+}
